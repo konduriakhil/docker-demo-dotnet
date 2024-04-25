44b3854f20807d11fcbfcb6ab884aab13ad20d72
shaikkhajaibrahim <qtkhajadevops@gmail.com>
2024-04-25 16:14:01 +0530

    Added changes

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+﻿FROM mcr.microsoft.com/dotnet/aspnet:8.0
+LABEL author="Shaikkhajaibrahim"
+ADD dist/ /myapp
+WORKDIR /myapp
+ENV ASPNETCORE_URLS="http://0.0.0.0:5000"
+CMD ["dotnet", "docker-demo-dotnet.dll"]
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace docker_demo_dotnet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
